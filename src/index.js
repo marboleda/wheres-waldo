04b41d744a7115b9ba9d9a709ae0eac219b6c82e
Marco Arboleda <arboleda.marco@gmail.com>
2021-02-01 18:50:39 -0500

    Initialize Firebase in app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7HLl5lgWXk1XFfdaprzmSC54D2SO5czs",
+  authDomain: "whereswaldo-6d58b.firebaseapp.com",
+  projectId: "whereswaldo-6d58b",
+  storageBucket: "whereswaldo-6d58b.appspot.com",
+  messagingSenderId: "132896620378",
+  appId: "1:132896620378:web:90b1831a52250e2df77d63"
+};
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
