e2c8123eb1d266b770bce0d60363e6a01c530de6
Marco Arboleda <arboleda.marco@gmail.com>
2021-02-04 19:24:35 -0500

    Fix faulty Firebase function call

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as firebase from 'firebase';
+import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = {
