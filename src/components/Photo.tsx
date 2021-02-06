acd13da8c4d05d29b3c24e18dc141a49c632dac6
Marco Arboleda <arboleda.marco@gmail.com>
2021-02-05 23:26:26 -0500

    Display photo

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import image1 from '../images/wheres-waldo-1.jpg'
+
+const Image = styled.img`
+`;
+
 const Photo = () => {
   return (
     <div className="Photo">
+        <Image src={image1}></Image>
     </div>
   );
 }
