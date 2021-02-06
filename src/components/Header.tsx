8bb494fe0d4932a75c3f43e6a7785864d74db021
Marco Arboleda <arboleda.marco@gmail.com>
2021-02-05 22:54:55 -0500

    Install styled-components type definitions
    TypeScript will not work with the styled-components React library without this

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    text-align: center;
+`;
+
+const Title = styled.h1`
+`;
+
+const Header = () => {
+    return (
+        <Container>
+            <Title>Where's Waldo</Title>
+        </Container>
+    )
+}
