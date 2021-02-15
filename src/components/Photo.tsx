1e85a89d4792a8ff4596cea8aed70f3e2baecc68
Marco Arboleda <arboleda.marco@gmail.com>
2021-02-14 22:00:01 -0500

    Capture x and y coordinates when clicking on the photo

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { MouseEvent } from 'react';
 import styled from 'styled-components';
 
 import image1 from '../images/wheres-waldo-1.jpg'
@@ -7,9 +7,22 @@ const Image = styled.img`
 `;
 
 const Photo = () => {
+
+  const handleClick = (e: MouseEvent) => {
+    const photo: HTMLElement | null = document.querySelector('.Photo');
+
+    if (photo) {
+      const x = e.pageX - photo.offsetLeft;
+      const y = e.pageY - photo.offsetTop;
+      console.log(x, y);
+    }
+  }
+
   return (
     <div className="Photo">
-        <Image src={image1}></Image>
+        <Image src={image1}
+               onClick={handleClick}>  
+        </Image>
     </div>
   );
 }
