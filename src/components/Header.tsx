8e74a0cea54774359219e31df70932fc57a7e964
Marco Arboleda <arboleda.marco@gmail.com>
2021-02-27 17:20:45 -0500

    Make header more visually appealing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import logo from '../images/wheres-waldo-logo.jpg';
+
 const Container = styled.div`
     text-align: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
 `;
 
 const Title = styled.h1`
+    font-family: 'Optima-ExtraBlack';
+    font-weight: bold;
+    display: inline;
 `;
+
+const TitleText = styled.span`
+    color: ${props => props.color}
+`;
+
+const Logo = styled.img`
+    max-height: 80px;
+`;
+
+
 
 const Header = () => {
     return (
         <Container>
-            <Title>Where's Waldo</Title>
+            <Title><TitleText color='#039be5'>WHERE'S</TitleText> <TitleText color='#7d1b1b'>WALDO?</TitleText></Title>
+            <Logo src={logo}/>
         </Container>
     )
 }
